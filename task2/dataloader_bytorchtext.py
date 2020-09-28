36ed37e3e98b47a5bb21b05a9a4c09408abbbf99
funnyZhu <funnyzhu1997@gmail.com>
2020-09-28 17:48:00 +0800

    add spacy tokenizer.

--- task2/dataloader_bytorchtext.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3.6
-# -*- coding: utf-8 -*-
-# @Time    : 2020/9/27 10:22 上午
-# @Author  : zbl
-# @Email   : funnyzhu1997@gmail.com
-# @File    : dataloader_bytorchtext.py
-# @Software: PyCharm
